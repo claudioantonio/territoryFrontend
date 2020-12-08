554706c53c6c7b5ca06f62a7b4daf78c66f3e05c
Cláudio <claudio.engdist@gmail.com>
2020-12-08 00:05:35 -0300

    Commit to deploy on vercel - #7

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 console.log(process.env);
-const API_URL = process.env.API_URL || 'http://localhost:3333';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3333';
 
 const api = axios.create({
     baseURL: API_URL,
