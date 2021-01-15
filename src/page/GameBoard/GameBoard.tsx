9aba63384fe48ec6b9f6b8252678165b5098f941
Cláudio <claudio.engdist@gmail.com>
2021-01-15 08:56:55 -0300

    #49 - removing unused import

--- src/page/GameBoard/GameBoard.tsx
+++ src/page/GameBoard/GameBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import api from '../../service/api';
 import socketIo from 'socket.io-client';
