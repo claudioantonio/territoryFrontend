5bf220e82cb7d67b70dc34cd14583153eac8ed99
Cláudio <claudio.engdist@gmail.com>
2021-01-15 09:42:32 -0300

    Fix #49

--- src/component/Board/GameBoard.tsx
+++ src/component/Board/GameBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Edge from '../../logic/Edge';
 import PlayData from '../../logic/PlayData';
 
